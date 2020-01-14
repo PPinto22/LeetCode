af73bab5b99ca4113509a381fffb7f292904bdf0
Pedro Pinto <pedro.pinto@xpand-it.com>
2020-01-14 16:36:42 +0000

    Problem 35
    C++ solution

---
+++ problems/0035. Search Insert Position/solution.cpp
@@ -0,0 +1,88 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int low = 0, high = nums.size() - 1;
+        while (low < high)
+        {
+            int mid = (low + high) / 2;
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+            else if (target < nums[mid])
+            {
+                high = mid;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return low == nums.size() - 1 && target > nums[low] ? low + 1 : low;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+                    return !isspace(ch);
+                }));
+}
+
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+                    return !isspace(ch);
+                })
+                    .base(),
+                input.end());
+}
+
+vector<int> stringToIntegerVector(string input)
+{
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim))
+    {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input)
+{
+    return stoi(input);
+}
+
+int main()
+{
+    string line;
+    while (getline(cin, line))
+    {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int target = stringToInteger(line);
+
+        int ret = Solution().searchInsert(nums, target);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
